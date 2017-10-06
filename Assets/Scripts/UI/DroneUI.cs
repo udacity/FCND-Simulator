fb20a507216987e186b7a3f73ac3f91c230473f1
Dom Luna <dluna132@gmail.com>
2017-10-06 13:41:19 -0400

    added minimap ui

--- Assets/Scripts/UI/DroneUI.cs
+++ Assets/Scripts/UI/DroneUI.cs
@@ -7,17 +7,28 @@ public class DroneUI : MonoBehaviour {
 	public Text _gpsText;
 	public Image _needleImage;
 	public Image _minimapImage;
+    public Camera _minimapCamera;
 	private QuadController _quadController;
+    private GameObject _droneObj;
+
 
 	void Awake () {
-		_quadController = GameObject.Find("Quad Drone").GetComponent<QuadController>();
+        _droneObj = GameObject.Find("Quad Drone");
+		_quadController = _droneObj.GetComponent<QuadController>();
+        UpdateMinimapCamPosition();
 	}
+
+    void UpdateMinimapCamPosition() {
+        var quadPos = _droneObj.transform.position;
+        _minimapCamera.transform.position = new Vector3(quadPos.x, quadPos.y + 30, quadPos.z);
+    }
 
 	void LateUpdate () {
         var lat = _quadController.getLatitude();
         var lon = _quadController.getLongitude();
         var alt = _quadController.getAltitude();
 		_gpsText.text = string.Format("Latitude = {0:0.000}\nLongitude = {1:0.000}\nAltitude = {2:0.000} (meters)", lat, lon, alt);
+        // _gpsText.color = new Color(255, 255, 255, 0);
 
         // North -> 0/360
         // East -> 90
@@ -27,5 +38,19 @@ public class DroneUI : MonoBehaviour {
         var oldHdg = _needleImage.rectTransform.rotation.eulerAngles.z;
         // rotate the needle by the yaw difference
         _needleImage.rectTransform.Rotate(0, 0, -(-hdg - -oldHdg));
+
+        // update minimap cam
+        UpdateMinimapCamPosition();
+
+        // display above sky camera frame on minimap
+        var targetTexture = _minimapCamera.targetTexture;
+        RenderTexture.active = targetTexture;
+        Texture2D texture2D = new Texture2D(targetTexture.width, targetTexture.height, TextureFormat.RGBA32, false);
+        texture2D.ReadPixels(new Rect(0, 0, targetTexture.width, targetTexture.height), 0, 0);
+        texture2D.Apply();
+        var sprite = Sprite.Create(texture2D, new Rect(0, 0, texture2D.width, texture2D.height), new Vector2(0.5f, 0.5f));
+        _minimapImage.sprite = Sprite.Create(texture2D, new Rect(0, 0, texture2D.width, texture2D.height), new Vector2(0.5f, 0.5f));
+        // Object.Destroy(texture2D);
+        // Object.Destroy(sprite);
 	}
 }
