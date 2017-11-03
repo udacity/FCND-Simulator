928f317420cdc02c14491807940d10cff08f57f4
Dom Luna <dluna132@gmail.com>
2017-11-03 14:55:43 -0400

    WIP: menus UI

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+namespace Menu
+{
+    public class MainMenu : MonoBehaviour
+    {
+        // project selection index
+        public int projectIndex;
+        public Text project_name;
+        public Image project_image;
+        public List<Project> projects;
+
+        public void Start()
+        {
+            projectIndex = 0;
+            var p = projects[projectIndex];
+            project_name.text = p.title;
+            project_image.sprite = p.image;
+        }
+
+        public void LoadControlMenu()
+        {
+            SceneManager.LoadScene("ControlMenu");
+        }
+
+        public void LoadMainMenu()
+        {
+            Debug.Log("go to main menu");
+            SceneManager.LoadScene("MenuScene");
+        }
+
+        public void SelectMode()
+        {
+            var p = projects[projectIndex];
+            SceneManager.LoadScene(p.scene.name);
+        }
+
+        public void Next()
+        {
+            projectIndex = (projectIndex + 1) % projects.Count;
+            var p = projects[projectIndex];
+            project_name.text = p.title;
+            project_image.sprite = p.image;
+        }
+
+        public void Previous()
+        {
+            if (projectIndex == 0)
+            {
+                projectIndex = projects.Count - 1;
+            }
+            else
+            {
+                projectIndex = (projectIndex - 1) % projects.Count;
+            }
+            projectIndex = (projectIndex + 1) % projects.Count;
+            var p = projects[projectIndex];
+            project_name.text = p.title;
+            project_image.sprite = p.image;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Menu/Project.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Menu
+{
+    public class Project : MonoBehaviour
+    {
+        public Scene scene;
+        public string title;
+        public Sprite image;
+        public string description;
+    }
+}
