246e249e37f483b128092c297fd1f6ff84fb081d
Dom Luna <dluna132@gmail.com>
2017-11-08 17:55:40 -0500

    prefabs and overlays yay

--- Assets/Scripts/Menu/Exit.cs
+++ Assets/Scripts/Menu/Exit.cs
@@ -1,14 +1,28 @@
 using UnityEngine;
-using UnityEngine.UI;
-using System.Collections;
 using UnityEngine.SceneManagement;
-using System.Collections.Generic;
 
 namespace Menu
 {
     // TODO: better name for this
     public class Exit : MonoBehaviour
     {
+        private GameObject controlsOverlay = null;
+
+        void Start()
+        {
+            var go = GameObject.Find("GCS");
+            var count = go.transform.childCount;
+            for (var i = 0; i < count; i++)
+            {
+                var name = go.transform.GetChild(i).gameObject.name;
+                if (name == "ControlsMenuOverlay")
+                {
+                    controlsOverlay = go.transform.GetChild(i).gameObject;
+                    break;
+                }
+            }
+        }
+
         void LateUpdate()
         {
             // Return to the main menu
@@ -17,21 +31,22 @@ namespace Menu
                 Debug.Log("Loading main menu");
                 SceneManager.LoadScene("MainMenu");
             }
-            // Reload current scene
             else if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
             {
+                // Reload current scene
                 if (Input.GetKeyDown(KeyCode.R))
                 {
                     Debug.Log("Resetting scene");
                     SceneManager.LoadScene(SceneManager.GetActiveScene().name);
                 }
-            }
-            // TODO: Toggle controls menu
-            // NOTE: not the same as loading the menu, which is done via
-            // the main menu
-            else if ("a" == "b")
-            {
-                Debug.Log("Toggling control menu");
+                // NOTE: not the same as loading the menu, which is done via
+                // the main menu
+                else if (controlsOverlay != null && Input.GetKeyDown(KeyCode.C))
+                {
+                    Debug.Log("Toggling control menu overlay");
+                    var active = controlsOverlay.activeSelf;
+                    controlsOverlay.SetActive(!active);
+                }
             }
         }
 
