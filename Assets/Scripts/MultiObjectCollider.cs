dd15ed311c8f6df13cf68cfe66bf408faee91c05
demi180 <noamweiss@gmail.com>
2017-11-22 15:58:43 -0800

    Added a dynamic multi-object collider script. Added colliders to objects in Urban scene

---
+++ Assets/Scripts/MultiObjectCollider.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class MultiObjectCollider : MonoBehaviour
+{
+	BoxCollider bc;
+
+	void OnTransformChildrenChanged ()
+	{
+		BuildCollider ();
+	}
+
+	void OnEnable ()
+	{
+		BuildCollider ();
+	}
+
+	void BuildCollider ()
+	{
+		bc = GetComponent<BoxCollider> ();
+		if ( bc == null )
+			bc = gameObject.AddComponent<BoxCollider> ();
+
+		var bounds = new Bounds ();
+		var rends = GetComponentsInChildren<Renderer> ();
+		foreach ( var r in rends )
+			bounds.center += r.bounds.center;
+		bounds.center /= rends.Length;
+		foreach ( var r in rends )
+			bounds.Encapsulate ( r.bounds );
+
+		bc.center = bounds.center - transform.position;
+		bc.size = bounds.size;
+	}
+}
