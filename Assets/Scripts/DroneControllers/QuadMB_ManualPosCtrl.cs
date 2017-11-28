e4743a4768dca6e8dfc208f404361c21a5a4f381
Steven Krukowski <spk170@stanford.edu>
2017-11-28 00:44:16 -0800

    Fixed vertical ascent wobble

--- Assets/Scripts/DroneControllers/QuadMB_ManualPosCtrl.cs
+++ Assets/Scripts/DroneControllers/QuadMB_ManualPosCtrl.cs
@@ -28,9 +28,12 @@ namespace MovementBehaviors
 			localPosition.x = controller.controller.GetLocalEast ();
 			Vector3 bodyVelocity = controller.controller.BodyVelocity;
 
+            Vector3 linearVelocity = controller.controller.LinearVelocity;
+            linearVelocity.x = linearVelocity.x * Mathf.Cos(pitchYawRoll.y) - linearVelocity.z * Mathf.Sin(pitchYawRoll.y);
+            linearVelocity.z = linearVelocity.x * Mathf.Sin(pitchYawRoll.y) + linearVelocity.z * Mathf.Cos(pitchYawRoll.y);
 
-			//Direct Control of the moments
-			Vector3 thrust = Vector3.zero;
+            //Direct Control of the moments
+            Vector3 thrust = Vector3.zero;
 			Vector3 yaw_moment = Vector3.zero;
 			Vector3 pitch_moment = Vector3.zero;
 			Vector3 roll_moment = Vector3.zero;
@@ -38,81 +41,95 @@ namespace MovementBehaviors
 
 			Vector3 velCmdBody = new Vector3 ( Input.GetAxis ( "Horizontal" ), Input.GetAxis ( "Thrust" ), Input.GetAxis ( "Vertical" ) );
 			float yawCmd = Input.GetAxis ( "Yaw" );
-			//Outer control loop for from a position/velocity command to a hdot, yaw rate, pitch, roll command
+            //Outer control loop for from a position/velocity command to a hdot, yaw rate, pitch, roll command
 
-			//If no control input provided (or in guided mode), use position hold
-/*			if ( velCmdBody.sqrMagnitude < controller.posctl_band )
-//			if ( Mathf.Sqrt ( Mathf.Pow ( velCmdBody.x, 2.0f ) + Mathf.Pow ( velCmdBody.y, 2.0f ) + Mathf.Pow ( velCmdBody.z, 2.0f ) ) < controller.posctl_band )
-			{
-				//Set position
-				if ( !controller.pos_set )
-				{
-					controller.posHoldLocal = localPosition;                            
-					controller.pos_set = true;
-//					Debug.Log ( controller.posHoldLocal );
-				}
+            //If no control input provided (or in guided mode), use position hold
+            /*			if ( velCmdBody.sqrMagnitude < controller.posctl_band )
+            //			if ( Mathf.Sqrt ( Mathf.Pow ( velCmdBody.x, 2.0f ) + Mathf.Pow ( velCmdBody.y, 2.0f ) + Mathf.Pow ( velCmdBody.z, 2.0f ) ) < controller.posctl_band )
+                        {
+                            //Set position
+                            if ( !controller.pos_set )
+                            {
+                                controller.posHoldLocal = localPosition;                            
+                                controller.pos_set = true;
+            //					Debug.Log ( controller.posHoldLocal );
+                            }
 
-				Vector3 posErrorLocal = controller.posHoldLocal - localPosition;
-				Vector3 velCmdLocal;
-				// print("Position Hold: " + posHoldLocal);
-				// print("Local Position: " + localPosition);
+                            Vector3 posErrorLocal = controller.posHoldLocal - localPosition;
+                            Vector3 velCmdLocal;
+                            // print("Position Hold: " + posHoldLocal);
+                            // print("Local Position: " + localPosition);
 
-				//Deadband around the position hold
-				if ( Mathf.Sqrt ( Mathf.Pow ( posErrorLocal.x, 2.0f ) + Mathf.Pow ( posErrorLocal.z, 2.0f ) ) < controller.posHoldDeadband )
-				{
-					velCmdLocal.x = 0.0f;
-					velCmdLocal.z = 0.0f;
-				} else
-				{
-					velCmdLocal.x = controller.Kp_pos * posErrorLocal.x;
-					velCmdLocal.z = controller.Kp_pos * posErrorLocal.z;
-				}
+                            //Deadband around the position hold
+                            if ( Mathf.Sqrt ( Mathf.Pow ( posErrorLocal.x, 2.0f ) + Mathf.Pow ( posErrorLocal.z, 2.0f ) ) < controller.posHoldDeadband )
+                            {
+                                velCmdLocal.x = 0.0f;
+                                velCmdLocal.z = 0.0f;
+                            } else
+                            {
+                                velCmdLocal.x = controller.Kp_pos * posErrorLocal.x;
+                                velCmdLocal.z = controller.Kp_pos * posErrorLocal.z;
+                            }
 
-				velCmdLocal.y = controller.Kp_alt * posErrorLocal.y;
+                            velCmdLocal.y = controller.Kp_alt * posErrorLocal.y;
 
 
-				//Rotate into the local heading frame
-				float cosYaw = Mathf.Cos ( pitchYawRoll.y );
-				float sinYaw = Mathf.Sin ( pitchYawRoll.y );
-				velCmdBody.x = cosYaw * velCmdLocal.x - sinYaw * velCmdLocal.z;
-				velCmdBody.z = sinYaw * velCmdLocal.x + cosYaw * velCmdLocal.z;
+                            //Rotate into the local heading frame
+                            float cosYaw = Mathf.Cos ( pitchYawRoll.y );
+                            float sinYaw = Mathf.Sin ( pitchYawRoll.y );
+                            velCmdBody.x = cosYaw * velCmdLocal.x - sinYaw * velCmdLocal.z;
+                            velCmdBody.z = sinYaw * velCmdLocal.x + cosYaw * velCmdLocal.z;
 
-				velCmdBody.y = velCmdLocal.y;
-			} else
-			{
-				controller.pos_set = false;
-			}*/
+                            velCmdBody.y = velCmdLocal.y;
+                        } else
+                        {
+                            controller.pos_set = false;
+                        }*/
 
-			//Heading hold if in guided mode or no input
-/*			if ( Mathf.Abs ( yawCmd ) <= 0.0f )
-			{
-				if ( !controller.yawSet )
-				{
-					controller.yawHold = pitchYawRoll.y;
-					controller.yawSet = true;
-				}
+            //Heading hold if in guided mode or no input
+            /*			if ( Mathf.Abs ( yawCmd ) <= 0.0f )
+                        {
+                            if ( !controller.yawSet )
+                            {
+                                controller.yawHold = pitchYawRoll.y;
+                                controller.yawSet = true;
+                            }
 
-				float yawError = controller.yawHold - pitchYawRoll.y;
-				if ( yawError > Mathf.PI )
-				{
-					yawError = yawError - 2.0f * Mathf.PI;
-				} else
-				if ( yawError < -1.0f * Mathf.PI )
-				{
-					yawError = yawError + 2.0f * Mathf.PI;
-				}
-				yawCmd = controller.Kp_yaw * yawError;
-			} else
-			{
-				controller.yawSet = false;
-			}*/
+                            float yawError = controller.yawHold - pitchYawRoll.y;
+                            if ( yawError > Mathf.PI )
+                            {
+                                yawError = yawError - 2.0f * Mathf.PI;
+                            } else
+                            if ( yawError < -1.0f * Mathf.PI )
+                            {
+                                yawError = yawError + 2.0f * Mathf.PI;
+                            }
+                            yawCmd = controller.Kp_yaw * yawError;
+                        } else
+                        {
+                            controller.yawSet = false;
+                        }*/
 
-			//Control loop from a body velocity command to a Hdot, yaw rate, pitch, and roll command
+            //Control loop from a body velocity command to a Hdot, yaw rate, pitch, and roll command
+            float yawError = 0.0f - pitchYawRoll.y;
+            if (yawError > Mathf.PI)
+            {
+                yawError = yawError - 2.0f * Mathf.PI;
+            }
+            else
+            if (yawError < -1.0f * Mathf.PI)
+            {
+                yawError = yawError + 2.0f * Mathf.PI;
+            }
+
+            yawCmd = controller.Kp_yaw * (0.0f - pitchYawRoll.y);
+
+            
 			Vector3 velocityErrorBody = Vector3.zero;
 			Vector3 velocityErrorBodyD = Vector3.zero;
-			velocityErrorBody.x = controller.moveSpeed * velCmdBody.x - bodyVelocity.x;
-			velocityErrorBody.z = controller.moveSpeed * velCmdBody.z - bodyVelocity.z;
-			velocityErrorBodyD = (velocityErrorBody - lastVelocityErrorBody) / Time.deltaTime;
+            velocityErrorBody.x = controller.moveSpeed * velCmdBody.x - linearVelocity.x;// (bodyVelocity.x);
+            velocityErrorBody.z = controller.moveSpeed * velCmdBody.z - linearVelocity.z;// (bodyVelocity.z);
+            velocityErrorBodyD = (velocityErrorBody - lastVelocityErrorBody) / Time.deltaTime;
 			lastVelocityErrorBody = velocityErrorBody;
 
 			angle_input[2] = controller.Kp_vel * velocityErrorBody.z + controller.Kd_vel * velocityErrorBodyD.z;
