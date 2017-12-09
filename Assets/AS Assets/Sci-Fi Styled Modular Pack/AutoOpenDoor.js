9b2a132088d1cdaef786078c67ede813aace6606
demi180 <noamweiss@gmail.com>
2017-12-08 18:34:39 -0800

    Import a bunch of AS assets, start setting up with MP-indoor2 scene
    Decided to use sci-fi pack instead of warehouse due to overall quality
    of assets

---
+++ Assets/AS Assets/Sci-Fi Styled Modular Pack/AutoOpenDoor.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+private var anim : Animator;
+var character : GameObject;
+var distanceToOpen:float = 5;
+private var characterNearbyHash : int = Animator.StringToHash("character_nearby");
+
+function Start () 
+{
+    anim = GetComponent("Animator");
+    if ( !character )
+    	enabled = false;
+}
+
+
+function Update () 
+{
+	var distance = Vector3.Distance(transform.position,character.transform.position);
+	
+	if (distanceToOpen>=distance){
+    	anim.SetBool(characterNearbyHash, true);
+    }else{
+    	anim.SetBool(characterNearbyHash, false);
+    }
+}
