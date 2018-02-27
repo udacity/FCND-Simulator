9fd066e774af322291cfced691b3ad8d3f1b78fc
demi180 <noamweiss@gmail.com>
2018-02-27 13:48:54 -0800

    Mapbox improvements. Added water, added clouds to all scenes, added reflection probe.

---
+++ Assets/Scripts/RProbeUpdater.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mapbox.Unity.Map;
+
+public class RProbeUpdater : MonoBehaviour
+{
+	ReflectionProbe probe;
+	Transform mapTransform;
+	float nextUpdate;
+
+	void Awake ()
+	{
+		GameObject mapObject = GameObject.Find ( "Map" );
+		if ( mapObject != null )
+		{
+			mapTransform = mapObject.transform;
+			mapObject.GetComponent<AbstractMap> ().OnInitialized += OnMapInitialized;
+		}
+		probe = GetComponent<ReflectionProbe> ();
+		nextUpdate = Time.time + 2;
+	}
+
+	void Update ()
+	{
+		if ( Time.time > nextUpdate )
+		{
+			probe.RenderProbe ();
+			nextUpdate = Time.time + 2;
+		}
+	}
+
+	void OnMapInitialized ()
+	{
+		transform.position = mapTransform.position + Vector3.up * 99;
+	}
+}
