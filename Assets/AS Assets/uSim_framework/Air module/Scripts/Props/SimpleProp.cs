ae03a3d959505ecd3dccaec2115332c385fd60df
Steven Krukowski <spk170@stanford.edu>
2018-07-18 15:52:55 -0700

    Added Cora scene:
    * Cessna replaced by Cora
    * Prop blur set by custom RPM
    * Blur slowly spins.

--- Assets/AS Assets/uSim_framework/Air module/Scripts/Props/SimpleProp.cs
+++ Assets/AS Assets/uSim_framework/Air module/Scripts/Props/SimpleProp.cs
@@ -42,6 +42,12 @@ public class SimpleProp : MonoBehaviour {
 
 	public float densityCoef = 1f;
 
+    public float rpmBlur;
+    public float maxRpm;
+
+    public Transform customTransform;
+  
+
     //Low pass time constant to add engine dynamics
     public float throttleAlpha = 0.1f;
 
@@ -52,19 +58,23 @@ public class SimpleProp : MonoBehaviour {
 
 	void FixedUpdate () {
 
-		//if (engineAttached == null)
-		//	return;
-		forcePoint = transform;
+        //if (engineAttached == null)
+        //	return;
+        if (!useCustomForcePoint)
+        {
+            customTransform = transform;
+            forcePoint = transform;
+        }
 
-		//thrustDir = engineAttached.thrustDir;
+        //thrustDir = engineAttached.thrustDir;
 
-		//thrustDir = Mathf.Clamp (thrustDir, -1f, 1f);
+        //thrustDir = Mathf.Clamp (thrustDir, -1f, 1f);
 
-		//inputForce = engineAttached.outputForce;
+        //inputForce = engineAttached.outputForce;
 
-		//rpm = engineAttached.rpm / reductionGear;
+        //rpm = engineAttached.rpm / reductionGear;
 
-		transform.Rotate (0f,0f,rpm /60f);
+        customTransform.Rotate (0f,0f,rpm /60f);
 
 		//frictionForce = frictionCurve.Evaluate (rpm / 1000f) * friction;
 
@@ -73,13 +83,15 @@ public class SimpleProp : MonoBehaviour {
 		//engineAttached.addedFriction = frictionForce;
 
 		if (targetBody != null)
-			targetBody.AddForceAtPosition (transform.TransformDirection (Vector3.forward) * outputForce, forcePoint.position);
+			targetBody.AddForceAtPosition (customTransform.TransformDirection (Vector3.forward) * outputForce, forcePoint.position);
 
-		if (rpm > 500) {
+		if (rpm > rpmBlur) {
 			propBlur.enabled = true;
+            float blurRate = -0.5f + (rpm - rpmBlur) / (maxRpm - rpmBlur);
+            propBlur.transform.Rotate(0f, 0f, blurRate);
 			propBlades.gameObject.SetActive (false);
 		}
-		if (rpm < 500) {
+		if (rpm < rpmBlur) {
 			propBlur.enabled = false;
 			propBlades.gameObject.SetActive (true);
 		}
