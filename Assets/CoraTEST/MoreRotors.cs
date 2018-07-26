649400605f04bae0b27b4dc51287a080d4c5bb88
demi180 <noamweiss@gmail.com>
2018-07-25 18:14:03 -0700

    -Fixed Cora flaps -Applied ScenarioManager changes

--- Assets/CoraTEST/MoreRotors.cs
+++ Assets/CoraTEST/MoreRotors.cs
@@ -13,30 +13,52 @@ public class MoreRotors : MonoBehaviour
 	public bool autoRotate;
 
 	public float rotationUpdateFreq = 0.2f;
+	public float rotationSpeed = 90;
+	public float tailRotationSpeed = 1200;
 
 	FastNoise fn;
-	float[] rotationUpdates;
+//	float[] rotationUpdates;
+	int[] blurredSigns;
 
 	void Awake ()
 	{
 		fn = new FastNoise ();
-		rotationUpdates = new float[blurredRotors.Length];
-		for ( int i = 0; i < rotationUpdates.Length; i++ )
-			rotationUpdates [ i ] = Random.value;
+//		rotationUpdates = new float[blurredRotors.Length];
+//		for ( int i = 0; i < rotationUpdates.Length; i++ )
+//			rotationUpdates [ i ] = Random.value;
+		if ( autoRotate )
+		{
+			cwRotors.ForEach ( x => x.GetComponent<MeshRenderer> ().enabled = false );
+			ccwRotors.ForEach ( x => x.GetComponent<MeshRenderer> ().enabled = false );
+		}
+		blurredSigns = new int[blurredRotors.Length];
+		for ( int i = 0; i < blurredRotors.Length; i++ )
+		{
+			Vector3 euler = blurredRotors [ i ].localEulerAngles;
+			euler.z = Random.value * 360f;
+			blurredRotors [ i ].localEulerAngles = euler;
+			if ( blurredRotors [ i ].GetComponent<MeshRenderer> ().sharedMaterial.name.Contains ( "blue" ) )
+				blurredSigns [ i ] = 1;
+			else
+				blurredSigns [ i ] = -1;
+		}
 	}
 
 	void LateUpdate ()
 	{
 		if ( autoRotate )
 		{
-			for ( int i = 0; i < rotationUpdates.Length; i++ )
+			float frameSpeed = rotationSpeed * Time.deltaTime;
+			for ( int i = 0; i < blurredRotors.Length; i++ )
 			{
-				if ( Time.time > rotationUpdates[i] )
-				{
-					blurredRotors [ i ].Rotate ( Vector3.forward * fn.GetSimplex ( Time.time, 0 ) * 10, Space.Self );
-					rotationUpdates [ i ] = Time.time + rotationUpdateFreq;
-				}
+				blurredRotors [ i ].Rotate ( Vector3.forward * frameSpeed * blurredSigns [ i ], Space.Self );
+//				if ( Time.time > rotationUpdates[i] )
+//				{
+//					blurredRotors [ i ].Rotate ( Vector3.forward * fn.GetSimplex ( Time.time, 0 ) * 10, Space.Self );
+//					rotationUpdates [ i ] = Time.time + rotationUpdateFreq;
+//				}
 			}
+			tailRotor.Rotate ( -Vector3.right * tailRotationSpeed * Time.deltaTime );
 //			Vector3 rot = Vector3.up * rotateSpeed * Time.deltaTime;
 //			for ( int i = 0; i < cwRotors.Length; i++ )
 //				cwRotors [ i ].Rotate ( rot, Space.Self );
@@ -45,5 +67,17 @@ public class MoreRotors : MonoBehaviour
 //			if ( tailRotor != null )
 //				tailRotor.RotateAround ( tailPivot.position, -tailPivot.right, tailRotorSpeed * Time.deltaTime );
 		}
+	}
+
+	public void Rotate (float speed, float tailSpeed)
+	{
+		Vector3 right = Vector3.forward * speed * Time.deltaTime;
+		Vector3 left = -Vector3.forward * speed * Time.deltaTime;
+		for ( int i = 0; i < cwRotors.Length; i++ )
+		{
+			cwRotors [ i ].Rotate ( right );
+			ccwRotors [ i ].Rotate ( left );
+		}
+		tailRotor.Rotate ( -Vector3.right * tailSpeed * Time.deltaTime );
 	}
 }
